c9061a4d572ba1dbf2c5cf7a32b4d858bb322be0
Felipe Jose Solorzano Michel <felipemichel147@gmail.com>
2024-07-23 14:07:16 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿public class Persona
+{
+    private string nombre;
+    private int edad;
+    private string direccion;
+    private string correoElectronico;
+
+    public void EnviarCorreoElectronico(string mensaje)
+    {
+        //Lógica para enviar correo electrónico
+    }
+
+    public void ImprimirDatos()
+    {
+        Console.WriteLine($"Nombre: {nombre}");
+        Console.WriteLine($"Edad: {edad}");
+        Console.WriteLine($"Dirección: {direccion}");
+        Console.WriteLine($"Correo Electrónico: {correoElectronico}");
+    }
+}
