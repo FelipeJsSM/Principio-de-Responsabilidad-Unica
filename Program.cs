968e8eb9e12f5f0b65e3877f54da37926beaee73
Felipe Jose Solorzano Michel <felipemichel147@gmail.com>
2024-07-23 14:27:44 -0400

    Refactor 1.0

--- Program.cs
+++ Program.cs
@@ -1,20 +1,29 @@
-﻿public class Persona
+﻿// Clase Persona centrada en la representación de datos
+public class Persona
 {
-    private string nombre;
-    private int edad;
-    private string direccion;
-    private string correoElectronico;
+    public string nombre { get; set; }
+    public int edad { get; set; }
+    public string direccion { get; set; }
+    public string correoElectronico { get; set; }
+}
 
-    public void EnviarCorreoElectronico(string mensaje)
+// Clase ServicioCorreo para la responsabilidad de enviar correos electrónicos
+public class ServicioCorreo
+{
+    public void EnviarCorreo(Persona destinatario, string mensaje)
     {
-        //Lógica para enviar correo electrónico
+        // Lógica para enviar correo electrónico
     }
+}
 
-    public void ImprimirDatos()
+// Clase Utilidad para la responsabilidad de imprimir datos
+public static class Utilidad
+{
+    public static void ImprimirDatos(Persona persona)
     {
-        Console.WriteLine($"Nombre: {nombre}");
-        Console.WriteLine($"Edad: {edad}");
-        Console.WriteLine($"Dirección: {direccion}");
-        Console.WriteLine($"Correo Electrónico: {correoElectronico}");
+        Console.WriteLine($"Nombre: {persona.nombre}");
+        Console.WriteLine($"Edad: {persona.edad}");
+        Console.WriteLine($"Dirección: {persona.direccion}");
+        Console.WriteLine($"Correo Electrónico: {persona.correoElectronico}");
     }
 }
